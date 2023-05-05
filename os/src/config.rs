7518b9e279abaa2437a5ccbcf6c5ec61c52b8778
kayoch1n <hanayolawk@gmail.com>
2023-05-05 16:46:16 +0800

    添加一个问题备注

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,6 +1,8 @@
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
-pub const MAX_APP_NUM: usize = 128;
+// PS: 调大这个值会有bug导致在loader::init加载第一个app的时候
+// 陷入死循环。调试发现在 __all_traps 处的内容也就是代码被覆盖掉了，变成一些垃圾数据
+pub const MAX_APP_NUM: usize = 64;
 
 /// 对比：内核第一条指令的地址是 0x80200000
 pub const APP_BASE_ADDRESS: usize = 0x80400000;
